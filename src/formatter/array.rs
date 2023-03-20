640680668976cfa9f5c5b6c04354179c2dc4b962
Seyyyy <mysdc7@gmail.com>
2023-03-20 23:35:19 +0900

    feat: add rgb option

--- src/formatter/array.rs
+++ src/formatter/array.rs
@@ -12,6 +12,16 @@ pub fn convert_2d_array(arr: &Vec<u32>, width: usize, height: usize) -> Vec<Vec<
     result
 }
 
+pub fn convert_flat_2d_array(arr: &Vec<Vec<u32>>) -> Vec<u32> {
+    let mut result = vec![0; arr.len() * 3];
+    for i in 0..arr.len() {
+        result[i * 3] = arr[i][0];
+        result[i * 3 + 1] = arr[i][1];
+        result[i * 3 + 2] = arr[i][2];
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,5 +31,12 @@ mod tests {
         let arr = vec![100, 100, 100, 0, 0, 0];
         let result = convert_2d_array(&arr, 2, 1);
         assert_eq!(result, vec![vec![100, 100, 100], vec![0, 0, 0]]);
+    }
+
+    #[test]
+    fn test_convert_flat_2d_array() {
+        let arr = vec![vec![100, 100, 100], vec![0, 0, 0]];
+        let result = convert_flat_2d_array(&arr);
+        assert_eq!(result, vec![100, 100, 100, 0, 0, 0]);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,12 @@ mod core;
 mod formatter;
 
 #[wasm_bindgen]
+pub enum ColorModel {
+    RGB,
+    HSV,
+}
+
+#[wasm_bindgen]
 pub struct Image {
     hsv: Vec<u32>,
     width: u32,
@@ -13,12 +19,31 @@ pub struct Image {
 #[wasm_bindgen]
 impl Image {
     #[wasm_bindgen(constructor)]
-    pub fn new(hsv: Vec<u32>, width: u32, height: u32) -> Image {
-        Image {
-            hsv,
-            width,
-            height,
-            usage_rate: core::simplify::UsageRate::new(),
+    pub fn new(hsv: Vec<u32>, width: u32, height: u32, color_model: ColorModel) -> Image {
+        match color_model {
+            ColorModel::RGB => {
+                let array2d =
+                    formatter::array::convert_2d_array(&hsv, width as usize, height as usize);
+                let hsv2darray: Vec<Vec<u32>> = array2d
+                    .iter()
+                    .map(|v| formatter::color_model::rgb2hsv(v[0], v[1], v[2]))
+                    .collect();
+                let hsv = formatter::array::convert_flat_2d_array(&hsv2darray);
+                return Image {
+                    hsv,
+                    width,
+                    height,
+                    usage_rate: core::simplify::UsageRate::new(),
+                };
+            }
+            ColorModel::HSV => {
+                return Image {
+                    hsv,
+                    width,
+                    height,
+                    usage_rate: core::simplify::UsageRate::new(),
+                };
+            }
         }
     }
 
@@ -59,7 +84,7 @@ mod tests {
 
     #[test]
     fn test_get_hue() {
-        let mut image = Image::new(vec![100, 100, 100, 0, 0, 0], 2, 1);
+        let mut image = Image::new(vec![100, 100, 100, 0, 0, 0], 2, 1, ColorModel::HSV);
         image.calc_usage_rate();
         assert_eq!(
             image.get_usage_rate_hue(),
@@ -69,14 +94,14 @@ mod tests {
 
     #[test]
     fn test_get_gray_scale() {
-        let mut image = Image::new(vec![100, 100, 100, 0, 0, 0], 2, 1);
+        let mut image = Image::new(vec![100, 100, 100, 0, 0, 0], 2, 1, ColorModel::HSV);
         image.calc_usage_rate();
         assert_eq!(image.get_usage_rate_gray_scale(), vec![1, 0, 0])
     }
 
     #[test]
     fn test_get_saturation() {
-        let mut image = Image::new(vec![100, 100, 100, 0, 0, 0], 2, 1);
+        let mut image = Image::new(vec![100, 100, 100, 0, 0, 0], 2, 1, ColorModel::HSV);
         image.calc_usage_rate();
         assert_eq!(
             image.get_usage_rate_saturation(),
@@ -86,11 +111,21 @@ mod tests {
 
     #[test]
     fn test_get_brightness() {
-        let mut image = Image::new(vec![100, 100, 100, 0, 0, 0], 2, 1);
+        let mut image = Image::new(vec![100, 100, 100, 0, 0, 0], 2, 1, ColorModel::HSV);
         image.calc_usage_rate();
         assert_eq!(
             image.get_usage_rate_brightness(),
             vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        )
+    }
+
+    #[test]
+    fn test_get_hue_rgb() {
+        let mut image = Image::new(vec![85, 255, 0, 0, 0, 0], 2, 1, ColorModel::RGB);
+        image.calc_usage_rate();
+        assert_eq!(
+            image.get_usage_rate_hue(),
+            vec![0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
         )
     }
 }
