7479e4dad12aaba4c0397b1aafb57b497a2a9e34
Seyyyy <mysdc7@gmail.com>
2023-03-26 01:44:07 +0900

    build: build target modified and released to web

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 import { ImageLoader } from "./Loader/Loader";
+import init from "lydie";
+import url from "lydie/lydie_bg.wasm?url";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      await init(url);
+    })();
+  }, []);
+
   return (
     <div>
       <h1>lydie sample</h1>
