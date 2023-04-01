0f3440aceed8ee5a34663649fa2119a42a1af8d8
Seyyyy <mysdc7@gmail.com>
2023-04-02 06:00:41 +0900

    build: fix to match 0.1.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-import init from "./pkg/lydie";
+import init, { ColorModel } from "./pkg/lydie";
 
 export default init;
 
@@ -7,9 +7,9 @@ export * from "./pkg/lydie";
 export class Lydie {
   public image: Image;
   constructor(
-    arr: number[],
+    imageArray: Uint32Array,
     width: number,
     height: number,
-    wasmMemory: WebAssembly.Memory
+    ColorModel: ColorModel
   );
 }
--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,10 @@
-import init, { Image, ColorModel } from "./pkg/lydie.js";
+import init, { Image } from "./pkg/lydie.js";
 export default init;
 
 export * from "./pkg/lydie.js";
 
 export class Lydie {
-  constructor(imageArray, width, height, wasmMemory) {
-    this.image = new Image(imageArray.length, width, height, ColorModel.RGB);
-    const ar = new Uint32Array(
-      wasmMemory.buffer,
-      this.image.hsv_pointer,
-      imageArray.length
-    );
-    ar.set(imageArray);
+  constructor(imageArray, width, height, ColorModel) {
+    this.image = new Image(imageArray, width, height, ColorModel);
   }
 }
