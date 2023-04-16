862ad274ced2d8085199e06b9cf1b698328d6176
Seyyyy <mysdc7@gmail.com>
2023-04-16 19:25:20 +0900

    ci: apply clippy

--- src/formatter/color_model.rs
+++ src/formatter/color_model.rs
@@ -1,61 +1,61 @@
-pub fn rgb2hsv(r: u16, g: u16, b: u16) -> Vec<u16> {
-    let r = r as f64 / 255.0;
-    let g = g as f64 / 255.0;
-    let b = b as f64 / 255.0;
-    let max = r.max(g).max(b);
-    let min = r.min(g).min(b);
-    let mut h = 0.0;
+// pub fn rgb2hsv(r: u16, g: u16, b: u16) -> Vec<u16> {
+//     let r = r as f64 / 255.0;
+//     let g = g as f64 / 255.0;
+//     let b = b as f64 / 255.0;
+//     let max = r.max(g).max(b);
+//     let min = r.min(g).min(b);
+//     let mut h = 0.0;
 
-    let mut s = 0.0;
-    let d = max - min;
-    if max != 0.0 {
-        s = d / max;
-    }
+//     let mut s = 0.0;
+//     let d = max - min;
+//     if max != 0.0 {
+//         s = d / max;
+//     }
 
-    let v = max;
+//     let v = max;
 
-    if max != min {
-        if min == r {
-            h = 60.0 * (b - g) / d + 180.0;
-        } else if min == g {
-            h = 60.0 * (r - b) / d + 300.0;
-        } else if min == b {
-            h = 60.0 * (g - r) / d + 60.0;
-        }
-    }
+//     if max != min {
+//         if min == r {
+//             h = 60.0 * (b - g) / d + 180.0;
+//         } else if min == g {
+//             h = 60.0 * (r - b) / d + 300.0;
+//         } else if min == b {
+//             h = 60.0 * (g - r) / d + 60.0;
+//         }
+//     }
 
-    vec![
-        h.round() as u16,
-        (s * 100.0).round() as u16,
-        (v * 100.0).round() as u16,
-    ]
-}
+//     vec![
+//         h.round() as u16,
+//         (s * 100.0).round() as u16,
+//         (v * 100.0).round() as u16,
+//     ]
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn test_rgb2hsv1() {
-        let result = rgb2hsv(168, 50, 82);
-        assert_eq!(result, vec![344, 70, 66]);
-    }
+//     #[test]
+//     fn test_rgb2hsv1() {
+//         let result = rgb2hsv(168, 50, 82);
+//         assert_eq!(result, vec![344, 70, 66]);
+//     }
 
-    #[test]
-    fn test_rgb2hsv2() {
-        let result = rgb2hsv(0, 0, 0);
-        assert_eq!(result, vec![0, 0, 0]);
-    }
+//     #[test]
+//     fn test_rgb2hsv2() {
+//         let result = rgb2hsv(0, 0, 0);
+//         assert_eq!(result, vec![0, 0, 0]);
+//     }
 
-    #[test]
-    fn test_rgb2hsv3() {
-        let result = rgb2hsv(255, 255, 255);
-        assert_eq!(result, vec![0, 0, 100]);
-    }
+//     #[test]
+//     fn test_rgb2hsv3() {
+//         let result = rgb2hsv(255, 255, 255);
+//         assert_eq!(result, vec![0, 0, 100]);
+//     }
 
-    #[test]
-    fn test_rgb2hsv4() {
-        let result = rgb2hsv(250, 12, 1);
-        assert_eq!(result, vec![3, 100, 98]);
-    }
-}
+//     #[test]
+//     fn test_rgb2hsv4() {
+//         let result = rgb2hsv(250, 12, 1);
+//         assert_eq!(result, vec![3, 100, 98]);
+//     }
+// }
