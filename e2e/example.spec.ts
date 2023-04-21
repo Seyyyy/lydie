f5aeaabeb1f69703405d6dd58164a024be799077
Seyyyy <mysdc7@gmail.com>
2023-04-22 05:28:08 +0900

    test: add e2e test for check performance regression

---
+++ e2e/example.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("パフォーマンステスト", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:4173/");
+    await page.getByText("image analyzed").innerHTML();
+  });
+
+  const checkPerf = async (ms: number, browserType: string) => {
+    switch (browserType) {
+      case "chromium":
+        await expect(ms).toBeLessThan(50);
+        break;
+      case "firefox":
+        await expect(ms).toBeLessThan(150);
+        break;
+      case "webkit":
+        await expect(ms).toBeLessThan(100);
+        break;
+    }
+  };
+
+  for (let i = 0; i < 5; i++) {
+    test(`calc usage rate: ${i}`, async ({ page, defaultBrowserType }) => {
+      const result = await page.locator("div#time").allInnerTexts();
+      checkPerf(parseFloat(result[0]), defaultBrowserType);
+    });
+  }
+});
+
+// test("パフォーマンスログ出力", async ({ page, context }) => {
+//   // start trace
+//   await context.tracing.start({ screenshots: true, snapshots: true });
+
+//   await page.goto("http://localhost:4173/");
+//   await page.getByRole("button", { name: "Click me" }).click();
+
+//   const testPerf = await page
+//     .getByText("image analyzed")
+//     .innerHTML({ timeout: 100 }); // 100ms以内に計算結果が表示されることを期待する
+//   await expect(testPerf).toBe("image analyzed");
+
+//   // stop trace
+//   await context.tracing.stop({ path: "trace.zip" });
+// });
